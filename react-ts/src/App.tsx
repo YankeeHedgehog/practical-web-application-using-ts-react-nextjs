0aad4fc925f920944a52bf85d4f2fe9e073c281e
YankeeHedgehog <yankee.hedgehog.610@gmail.com>
2022-11-02 15:02:00 +0900

    Create sample using useMemo and useCallback

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,16 +1,25 @@
-
+import { useState } from 'react'
 import { Counter, DoubleCounter } from './ch03/react-hooks/stateHooks'
-import { Parent } from './ch03/react-hooks/memoHooks'
+import { Parent, Parent2 } from './ch03/react-hooks/memoHooks'
+import UseMemoSample from './ch03/react-hooks/UseMemoHooks'
 import './App.css'
 
 function App() {
-
+  const [component, setComponent] = useState(
+    <DoubleCounter initialValue={0} />
+  )
 
   return (
     <div className="App">
-      <Counter initialValue={0} />
-      <DoubleCounter initialValue={0} />
-      <Parent />
+      <div className="tab" style={{ minWidth: '500px', minHeight: '500px' }}>
+        <button onClick={() => setComponent(<DoubleCounter initialValue={0} />)}>state</button>
+        <button onClick={() => setComponent(<><Parent /><Parent2 /></>)}>memo</button>
+        <button onClick={() => setComponent(<UseMemoSample/>)}>useMemo</button>
+        <hr />
+        <div className="selected-component">
+          {component}
+        </div>
+      </div>
     </div>
   )
 }
--- react-ts/src/ch03/react-hooks/memoHooks.tsx
+++ react-ts/src/ch03/react-hooks/memoHooks.tsx
@@ -21,6 +21,7 @@ export const Parent = () => {
   const isFizz = count % 3 === 0
   const isBuzz = count % 5 === 0
 
+  console.log(`Parent is called... `)
   return (
     <>
       <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
@@ -30,6 +31,45 @@ export const Parent = () => {
         <Fizz isFizz={isFizz} />
         <Buzz isBuzz={isBuzz} />
       </p>
+    </>
+  )
+}
+
+type ButtonProps = {
+  onClick: () => void
+}
+
+const DecrementButton = ({ onClick }: ButtonProps) => {
+  console.log('Decrement button is called...')
+
+  return <button onClick={onClick}>Decrement</button>
+}
+
+const IncrementButton = memo(({ onClick }: ButtonProps) => {
+  console.log('Increment button is called...')
+
+  return <button onClick={onClick}>Increment</button>
+})
+
+const DoubleButton = memo(({ onClick }: ButtonProps) => {
+  console.log('Double button is called...')
+
+  return <button onClick={onClick}>Double</button>
+})
+
+export const Parent2 = () => {
+  const [count, setCount] = useState(0)
+  const decrement = () => setCount(prevCount => prevCount - 1)
+  const increment = () => setCount(prevCount => prevCount + 1)
+  const double = useCallback(() => { setCount(prevCount => prevCount * 2) }, [])
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <DecrementButton onClick={decrement} />
+      <IncrementButton onClick={increment} />
+      {/* useCallback can use in memorized component */}
+      <DoubleButton onClick={double} />
     </>
   )
 }
